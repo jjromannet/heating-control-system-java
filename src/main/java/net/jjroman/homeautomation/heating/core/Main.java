b13a82722955e1668b4ea429a043dce1db508158
Jan Roman <mail@jjroman.net>
2015-02-08 21:24:09 +0000

    Initial commit of project setup

---
+++ src/main/java/net/jjroman/homeautomation/heating/core/Main.java
@@ -0,0 +1,12 @@
+package net.jjroman.homeautomation.heating.core;
+
+/**
+ * Main class to run application
+ *
+ * Created by Jan on 08/02/2015.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.print("Hello World!");
+    }
+}
